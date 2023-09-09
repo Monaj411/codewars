4dd70160e22973025c1d88a777436a355c336c2a
Monaj411 <100786191+Monaj411@users.noreply.github.com>
2023-09-09 07:41:13 +0100

    js code

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// write a function that takes an array of words and smashes them together into a sentence and returns the setence. you can ignore any need to sanitize words or add punctuation, but you should add
+
+// const smash = words => words.join('i love coding')
+// let smash = words => words.join("")
+// function smash() {
+//   const word = ["I", "Love", "coding"];
+//   const smash = word.join('  ')
+//   return smash
+// } 
+// console.log(smash())
+
+// Given a random non-negative number, you have to return the digit of this number within an array in reverse ondragover.
+function digitize() {
+  const n = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+  const digitize = n.reverse(); 
+  return digitize
+}
+digitize()
